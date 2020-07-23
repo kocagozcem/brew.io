e0418e9bf6621dcf176bd9222adbb2658850280e
Cem Kocagöz <kocagozcem@gmail.com>
2020-07-23 22:30:59 +0300

    works like a charm 🎉🎉

--- android/app/src/main/java/com/startingpack/MainActivity.java
+++ android/app/src/main/java/com/startingpack/MainActivity.java
@@ -1,4 +1,4 @@
-package com.startingpack;
+package com.projectx;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "startingpack";
+    return "project-x";
   }
 }
--- ios/startingpackTests/startingpackTests.m
+++ ios/startingpackTests/startingpackTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface startingpackTests : XCTestCase
+@interface project-xTests : XCTestCase
 
 @end
 
-@implementation startingpackTests
+@implementation project-xTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
