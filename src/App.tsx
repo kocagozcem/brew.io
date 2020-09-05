688f65ab5fd1c137a2f356dbf9d87c7fbab937ee
Cem Kocagöz <kocagozcem@gmail.com>
2020-09-05 04:25:21 +0300

    Added: Redux provider and store to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducers/index';
 import HomeScreen from './pages/homescreen';
 import RecipeDetailsScreen from './pages/recipe-details-screen';
 
 declare const global: {HermesInternal: null | {}};
+const store = createStore(reducer);
 
 const myTheme = {
   ...DefaultTheme,
@@ -31,16 +35,18 @@ const transparentHeaderOption = {
 
 const App = () => {
   return (
-    <NavigationContainer theme={myTheme}>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" options={noHeaderOption} component={HomeScreen} />
-        <Stack.Screen
-          name="Details"
-          options={transparentHeaderOption}
-          component={RecipeDetailsScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer theme={myTheme}>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" options={noHeaderOption} component={HomeScreen} />
+          <Stack.Screen
+            name="Details"
+            options={transparentHeaderOption}
+            component={RecipeDetailsScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
