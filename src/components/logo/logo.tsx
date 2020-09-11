2ec3ee899ad27d5a5ddb5393f24e991457d12136
Cem Kocagöz <kocagozcem@gmail.com>
2020-09-12 00:17:03 +0300

    Fix: Logo style

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import {Text, StyleSheet} from 'react-native';
 
-function Logo() {
-  return <Text style={styles.logo}>Brew.io</Text>;
+interface Props {
+  fontSize: number;
+}
+
+function Logo(props: Props) {
+  const {fontSize} = props;
+  const size: number = fontSize !== undefined ? fontSize : 22;
+  return <Text style={[styles.logo, {fontSize: size}]}>Brew.io</Text>;
 }
 
 const styles = StyleSheet.create({
