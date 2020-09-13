dfaa388f7c2ed54c7070ee9f10f23c2cbbd1d7b5
Cem Kocagöz <kocagozcem@gmail.com>
2020-09-14 02:25:03 +0300

    Added: Redux action to add recipe

--- src/reducers/recipe.ts
+++ src/reducers/recipe.ts
@@ -1,11 +1,20 @@
 import {Action} from '../models/action';
 import mockData from '../mock/mock-recipe.json';
+import {Recipe} from '../models/recipe';
 
 export default (state = {recipes: mockData}, action: Action) => {
   switch (action.type) {
     case 'SET_RECIPE':
       return {...state, recipes: action.value};
+    case 'ADD_RECIPE':
+      return {...state, recipes: prepareRecipeToAdd(state.recipes, action.value)};
     default:
       return state;
   }
 };
+
+function prepareRecipeToAdd(recipes: Recipe[], value: Recipe) {
+  const recipeList: Recipe[] = recipes;
+  recipeList.push(value);
+  return recipeList;
+}
